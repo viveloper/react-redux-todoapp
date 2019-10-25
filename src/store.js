faf81dab1cbf43fddb42e891d620c98c824f8613
unknown <usik1234@gmail.com>
2019-10-25 12:36:21 +0900

    using localStorage

--- src/store.js
+++ src/store.js
@@ -10,46 +10,56 @@ const initialState = {
 
 const store = createStore(rootReducer, initialState);
 
-const preData = {
-    todos: [
-        {
-            text: 'React Hooks',
-            date: '2019-10-22',
-            completed: true
-        },
-        {
-            text: 'Flux Architecture',
-            date: '2019-10-23',
-            completed: true
-        },
-        {
-            text: 'Redux Fundamentals',
-            date: '2019-10-24',
-            completed: true
-        },
-        {
-            text: 'Container and Presentational Component',
-            date: '2019-10-24',
-            completed: true
-        },
-        {
-            text: 'React-Redux',
-            date: '2019-10-24',
-            completed: false
-        },
-        {
-            text: 'Redux-Thunk',
-            date: '2019-10-24',
-            completed: false
-        }
-    ],    
-    filter: 'SHOW_ALL'
-}
-
 setTimeout(() => {
-    preData.todos.forEach(todo => {
+    if (!localStorage.getItem('TodoAppState')) {
+        const sampleData = {
+            todos: [
+                {
+                    text: 'React Hooks',
+                    date: '2019-10-22',
+                    completed: true
+                },
+                {
+                    text: 'Flux Architecture',
+                    date: '2019-10-23',
+                    completed: true
+                },
+                {
+                    text: 'Redux Fundamentals',
+                    date: '2019-10-24',
+                    completed: true
+                },
+                {
+                    text: 'Container and Presentational Component',
+                    date: '2019-10-24',
+                    completed: true
+                },
+                {
+                    text: 'React-Redux',
+                    date: '2019-10-24',
+                    completed: false
+                },
+                {
+                    text: 'Redux-Thunk',
+                    date: '2019-10-24',
+                    completed: false
+                }
+            ],
+            filter: 'SHOW_ALL'
+        }
+    
+        localStorage.setItem('TodoAppState', JSON.stringify(sampleData));
+    }    
+
+    const data = JSON.parse(localStorage.getItem('TodoAppState'));
+    data.todos.forEach(todo => {
         store.dispatch(addTodo(todo));
     });
 }, 300);
+
+store.subscribe(() => {
+    localStorage.setItem('TodoAppState', JSON.stringify(store.getState()));
+    console.log('store - sync localStorage')
+});
 
 export default store;
